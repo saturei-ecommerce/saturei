91728440d45eb12d14fb119bfd69a4f5faba7fff
Lucas Vieira <lucasvieirasilva.lpta@gmail.com>
2026-04-08 21:32:36 -0300

    add saturei-frontend and saturei-backend initial setup

---
+++ saturei-backend/src/main/java/com/saturei/backend/SatureiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.saturei.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatureiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatureiBackendApplication.class, args);
+	}
+
+}
